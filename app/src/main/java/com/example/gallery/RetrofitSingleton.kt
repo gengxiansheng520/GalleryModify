f39671d955d86bad045ebe292c0fe398db4cd65e
huangcan <291282209@qq.com>
2020-10-22 18:30:25 +0800

    删除不必要import

--- app/src/main/java/com/example/gallery/RetrofitSingleton.kt
+++ app/src/main/java/com/example/gallery/RetrofitSingleton.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
-import java.util.HashMap
+import java.util.*
 
 class RetrofitSingleton private constructor(){
 
@@ -27,7 +27,6 @@ class RetrofitSingleton private constructor(){
     }
 }
 interface ApiService {
-    //https://pixabay.com/api/?key=1d61&q=flower&per_page=100
     @GET("api/")
     fun getPhoto(@QueryMap params: HashMap<String, String>): Call<Pixabay?>?
 
